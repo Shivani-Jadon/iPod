6783e4850d0b859eb1488aa3296da5512410f3eb
Shivani Jadon <shivanijadon98@gmail.com>
2020-09-12 16:42:05 +0530

    Updates

---
+++ precache-manifest.36437ab05e8099081fc15d2991900ff4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c25b11d6a29e47344b7c19fb236fc995",
+    "url": "/iPod/index.html"
+  },
+  {
+    "revision": "4201ba9b1a71dcc7b4ba",
+    "url": "/iPod/static/css/main.191f6c0d.chunk.css"
+  },
+  {
+    "revision": "389b943b148d923b84b9",
+    "url": "/iPod/static/js/2.a0883003.chunk.js"
+  },
+  {
+    "revision": "3b6069e01f0db714f9e787b4dbe9775c",
+    "url": "/iPod/static/js/2.a0883003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4201ba9b1a71dcc7b4ba",
+    "url": "/iPod/static/js/main.bfab2f67.chunk.js"
+  },
+  {
+    "revision": "97dba56fde2dd1bc0e68",
+    "url": "/iPod/static/js/runtime-main.3a9395a1.js"
+  }
+]);
