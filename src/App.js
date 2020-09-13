99efa1b7f4271a6649cce4f7ec96722a0b3b5033
Shivani Jadon <shivanijadon98@gmail.com>
2020-09-13 17:32:50 +0530

    create methods for changing theme and wallpaper

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import Screen from './Screen';
 import Controls from './Controls';
 import ZingTouch from 'zingtouch';
+// importing wallpapers
+import wallpaper1 from './assets/images/wallpaper2.jpg';
+import wallpaper2 from './assets/images/wallpaper1.jpg';
+import wallpaper3 from './assets/images/wallpaper3.jpg';
+import wallpaper4 from './assets/images/wallpaper4.jpg';
+import wallpaper5 from './assets/images/wallpaper5.jpg';
 
 
 class App extends React.Component{
@@ -50,6 +56,8 @@ class App extends React.Component{
 	//funtion to move over the menu items
 	move = (changeMenu_State) => {
 		
+		if(this.state.screen >= 1)
+		{
 			const containerElement = document.getElementById('wheel');
 			const activeRegion = ZingTouch.Region(containerElement);
 
@@ -80,6 +88,7 @@ class App extends React.Component{
 				
 			});
 
+		}
     }
 	
 	// function to go into sub-menus
@@ -97,12 +106,69 @@ class App extends React.Component{
 		if (this.state.screen > 0) {
 			this.setState({
 				screen: this.state.screen - 1,
-				menu: 0,
 				menuItem: 0
+			})
+		}
+		if (this.state.screen > 0 && this.state.menu <= 2) {
+			this.setState({
+				menu : 0
 			})
 		}
 	}
 
+
+	// function for changing themes
+	changeTheme = () => {
+		if(this.state.screen === 3 && this.state.menu === 3 && this.state.menuItem === 0 ){
+			
+			const select_btn = document.getElementById('#okay-btn'); 
+
+			select_btn.addEventListener('click', function(){
+				let selected_theme = this.state.menuItem;
+
+				if(selected_theme === 0)
+				{
+					document.getElementsByClassName('ipod-frame').style.background = 'black';
+					document.getElementById('control').style.background = 'grey';
+					document.getElementById('control').style.color = 'white';
+				}else{
+					document.getElementsByClassName('ipod-frame').style.background = 'grey';
+					document.getElementById('control').style.background = 'white';
+					document.getElementById('control').style.color = 'grey';
+				}
+			})
+		}
+	}
+
+	// function for changing wallpaper
+	changeWallpaper = () => {
+
+		if(this.state.screen === 3 && this.state.menu === 3 && this.state.menuItem === 1 ){
+
+			const select_btn = document.getElementById('#okay-btn'); 
+
+			select_btn.addEventListener('click', function(){
+
+				let selected_wallpaper = this.state.menuItem;
+
+				switch(selected_wallpaper)
+				{
+					case 0 : document.getElementById('ipod-screen').style.backgroundImage = `url(${wallpaper1})`;
+							break;
+					case 1 : document.getElementById('ipod-screen').style.backgroundImage = `url(${wallpaper2})`;
+							break;
+					case 2 : document.getElementById('ipod-screen').style.backgroundImage = `url(${wallpaper3})`;
+							break;
+					case 3 : document.getElementById('ipod-screen').style.backgroundImage = `url(${wallpaper4})`;
+							break;
+					case 4 : document.getElementById('ipod-screen').style.backgroundImage = `url(${wallpaper5})`;
+							break;
+					default : document.getElementById('ipod-screen').style.backgroundImage = `url(${wallpaper1})`;
+							break;
+				}
+			})
+		}
+	}
 
 
   render(){
